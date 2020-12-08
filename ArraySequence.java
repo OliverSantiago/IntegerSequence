7c42ffd8a8c4f5f8d8c2f86924cd363e9c9408ce
Oliver Santiago <oliversantiago718@gmail.com>
2020-12-08 14:17:37 -0500

    first step

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,8 +1,33 @@
 import java.util.NoSuchElementException;
+import java.util.Arrays;
 public class ArraySequence implements IntegerSequence{
-  int currentIndex;
-  int []data;
+  private int currentIndex;
+  private int[] data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int[] other){
+    currentIndex = 0;
+    data = new int[other.length];
+    for (int i = 0; i < other.length; i++){
+      data[i]=other[i];
+    }
+  }
+
+  public void reset(){
+    currentIndex=0;
+  }
+
+  public int length(){
+    return data.length;
+  }
+
+  public boolean hasNext(){
+    return (currentIndex<this.length());
+  }
+
+  public int next(){
+    int temp = data[currentIndex];
+    currentIndex++;
+    return temp;
+  }
 }
