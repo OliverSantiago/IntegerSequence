ba5464ea01c27409ed9c7c9346435cb28a303916
Oliver Santiago <oliversantiago718@gmail.com>
2020-12-08 16:12:09 -0500

    Tests

--- Tester.java
+++ Tester.java
@@ -10,11 +10,20 @@ public class Tester{
   //   }
   //   System.out.println();
   // }
-  int[]nums = {1,3,5,0,-1,3,9};
-    IntegerSequence as = new ArraySequence(nums);
-    System.out.println("ArraySequence(array):");
-    while(as.hasNext()){
-      System.out.print(as.next()+", ");
-    }
+  // int[]nums = {1,3,5,0,-1,3,9};
+  //   IntegerSequence as = new ArraySequence(nums);
+  //   System.out.println("ArraySequence(array):");
+  //   while(as.hasNext()){
+  //     System.out.print(as.next()+", ");
+  //   }
+  //   System.out.println(as.next());
+  IntegerSequence r = new Range(10,20);
+  IntegerSequence as = new ArraySequence(r);
+
+  System.out.println("ArraySequence(seq):");
+  while(as.hasNext()){
+    System.out.print(as.next()+", ");
+  }
+  System.out.println();
   }
 }
